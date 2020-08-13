ca0c616b00546b2a5bf65dfa72013887d1ccef7c
arian <atahiraj@student.ulg.ac.be>
2020-08-13 23:21:29 +0200

    best and worst

---
+++ jobs/best.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Slurm arguments
+#
+#SBATCH --job-name=ADDTC            # Name of the job 
+#SBATCH --export=ALL                # Export all environment variables
+#SBATCH --output=/home/arian/Deep-Learning-August/results/resnet152_e20_batch_32_OneCycle_best.log
+#SBATCH --cpus-per-task=4           # Number of CPU cores to allocate
+#SBATCH --mem-per-cpu=4G            # Memory to allocate per allocated CPU core
+#SBATCH --gres=gpu:1                # Number of GPU's
+#SBATCH --time=2-15:00:00              # Max execution time
+#
+
+# Activate your Anaconda environment
+conda activate deep
+
+LOG_DIR=/home/arian/Deep-Learning-August/results/OneCycle_best
+
+mkdir $LOG_DIR
+
+
+# Run your Python script
+cd /home/arian/Deep-Learning-August
+python main.py --gpu --workers 4 --batch 32 --epoch 100 --model resnet152 \
+	--save $LOG_DIR/resnet152_e20_batch_32_OneCycle_best.pt \
+	--data_path /scratch/users/arian/data --json_path /scratch/users/arian/data/train.json \
+	--save_log $LOG_DIR --no_bias --wdecay 0.0005 --nesterov
