e00afa84e843a27f204dfd81544470c4a49594a3
arian <atahiraj@student.ulg.ac.be>
2020-08-16 14:45:57 +0200

    KEK

--- models/misc.py
+++ models/misc.py
@@ -4,7 +4,9 @@ import torchvision.models as models
 from collections import namedtuple
 import torch.optim as optim
 
-
+"""
+    Since we removed all the models but MobileNet, we should rework this function
+"""
 def get_all_model(
     model_name, output_dim,
 ):
@@ -18,21 +20,9 @@ def get_all_model(
     :return model: The model
     :rtype: nn.Module
     """
-    # Getting the model
+
     if model_name == "mobilenet_v2":
         model = models.mobilenet_v2(pretrained=True)
-        IN_FEATURES = model.classifier[-1].in_features
-        fc = nn.Linear(IN_FEATURES, output_dim,)
-        model.classifier[-1] = fc
-
-    elif model_name == "mnasnet0_5":
-        model = models.mnasnet0_5(pretrained=True)
-        IN_FEATURES = model.classifier[-1].in_features
-        fc = nn.Linear(IN_FEATURES, output_dim,)
-        model.classifier[-1] = fc
-
-    elif model_name == "mnasnet1_0":
-        model = models.mnasnet1_0(pretrained=True)
         IN_FEATURES = model.classifier[-1].in_features
         fc = nn.Linear(IN_FEATURES, output_dim,)
         model.classifier[-1] = fc
