abc78050ad505a4c0ef414e410e9d607355b81cf
arian <atahiraj@student.ulg.ac.be>
2020-08-16 14:42:20 +0200

    Cleanup

--- models/misc.py
+++ models/misc.py
@@ -25,18 +25,6 @@ def get_all_model(
         fc = nn.Linear(IN_FEATURES, output_dim,)
         model.classifier[-1] = fc
 
-    elif model_name == "resnext50_32x4d":
-        model = models.resnext50_32x4d(pretrained=True)
-        IN_FEATURES = model.fc.in_features
-        fc = nn.Linear(IN_FEATURES, output_dim,)
-        model.fc = fc
-
-    elif model_name == "resnext101_32x8d":
-        model = models.resnext101_32x8d(pretrained=True)
-        IN_FEATURES = model.fc.in_features
-        fc = nn.Linear(IN_FEATURES, output_dim,)
-        model.fc = fc
-
     elif model_name == "mnasnet0_5":
         model = models.mnasnet0_5(pretrained=True)
         IN_FEATURES = model.classifier[-1].in_features
